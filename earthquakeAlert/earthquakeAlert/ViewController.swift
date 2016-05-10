af0bc6a5f43bad4b7ac7bc87de55089216c7983b
1361579531@qq.com <1361579531@qq.com>
2016-05-10 14:07:16 +0800

    add alert

---
+++ earthquakeAlert/earthquakeAlert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  earthquakeAlert
+//
+//  Created by Linyou-IOS-1 on 16/5/10.
+//  Copyright © 2016年 Linyou-IOS-1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
